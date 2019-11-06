8221d50959caf0a0253268717f951f4960e6f95d
Interindep <Interindep@DESKTOP-29VVM8O>
2019-11-06 18:01:39 +0100

    Selection en plus

--- oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalAngencyTree.java
+++ oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalAngencyTree.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
+import javax.net.ssl.ExtendedSSLSession;
 
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 
@@ -17,11 +23,20 @@ import oliv.exo.rental.ui.RentalUIConstantes;
 public class RentalAngencyTree implements RentalUIConstantes{
 
 	@PostConstruct
-	public void initArbre(Composite parent, @Named(AGENCE_COURANTE) RentalAgency agence,IEclipseContext context) {
+	public void initArbre(Composite parent, @Named(AGENCE_COURANTE) RentalAgency agence,IEclipseContext context,ESelectionService selectionService) {
 		TreeViewer tv  = new TreeViewer(parent);
 		RentalProvider rp=ContextInjectionFactory.make(RentalProvider.class,context);
 		tv.setContentProvider(rp);
 		tv.setLabelProvider(rp);
 		tv.setInput(Arrays.asList(agence));
+		tv.addSelectionChangedListener(new ISelectionChangedListener() {
+			
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				IStructuredSelection sel = (IStructuredSelection)event.getSelection();
+				
+				selectionService.setSelection(sel.size()==1?sel.getFirstElement():sel.toArray());
+			}
+		});
 	}
 }
--- oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalPartView.java
+++ oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalPartView.java
@@ -1,25 +1,22 @@
 package oliv.exo.rental.ui.view;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
-import org.eclipse.swt.widgets.Scale;
-import org.eclipse.swt.widgets.Slider;
-import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
 
 import com.opcoach.training.rental.Rental;
@@ -27,8 +24,7 @@ import com.opcoach.training.rental.RentalAgency;
 
 import oliv.exo.rental.ui.RentalUIConstantes;
 
-public class RentalPartView implements RentalUIConstantes{
-	
+public class RentalPartView implements RentalUIConstantes {
 
 	private Label lblObjet;
 	private Label lblNom;
@@ -36,122 +32,115 @@ public class RentalPartView implements RentalUIConstantes{
 	private Label lblDatefin;
 	private Label lblPrenom;
 	private Group group;
-	private Text text;	
+	private Text text;
 	private Table table;
 
 	@PostConstruct
 	public void creationView(Composite parent, @Named(AGENCE_COURANTE) RentalAgency agence) {
 		parent.setLayout(new GridLayout(1, false));
-		Group infogroupe= new Group(parent,SWT.NONE);
+		Group infogroupe = new Group(parent, SWT.NONE);
 		infogroupe.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		infogroupe.setText("Information");
-		infogroupe.setLayout(new GridLayout(3,false));
-		
+		infogroupe.setLayout(new GridLayout(3, false));
+
 		lblObjet = new Label(infogroupe, SWT.NONE);
 		lblObjet.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
 		lblObjet.setText("Objet");
-		
-		Label  rentedObjectLabel = new Label(infogroupe,SWT.NONE);
+
+		Label rentedObjectLabel = new Label(infogroupe, SWT.NONE);
 		rentedObjectLabel.setText("Loué à : ");
-		
+
 		lblPrenom = new Label(infogroupe, SWT.NONE);
 		lblPrenom.setText("Prenom");
-		
+
 		lblNom = new Label(infogroupe, SWT.NONE);
 		lblNom.setText("Nom");
-		
+
 		Group grpDatesDeLocation = new Group(parent, SWT.NONE);
 		grpDatesDeLocation.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		grpDatesDeLocation.setText("Dates de location");
 		grpDatesDeLocation.setLayout(new GridLayout(2, false));
-		
+
 		Label lblDu = new Label(grpDatesDeLocation, SWT.NONE);
 		lblDu.setText("du :");
-		
+
 		lblDatedebut = new Label(grpDatesDeLocation, SWT.NONE);
 		lblDatedebut.setText("dateDebut");
-		
+
 		Label lblAu = new Label(grpDatesDeLocation, SWT.NONE);
 		lblAu.setText("au :");
-		
+
 		lblDatefin = new Label(grpDatesDeLocation, SWT.NONE);
 		lblDatefin.setText("dateFin");
-		
-		group = new Group(parent, SWT.NONE);
-		group.setLayout(new GridLayout(6, false));
-		group.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
-		Label lblNewLabel = new Label(group, SWT.NONE);
-		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-		lblNewLabel.setText("New Label");
-		
-		text = new Text(group, SWT.BORDER);
-		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		new Label(group, SWT.NONE);
-		
-		Combo combo = new Combo(group, SWT.NONE);
-		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
-		Button btnNewButton = new Button(group, SWT.NONE);
-		btnNewButton.setText("New Button");
-		
-		Button btnCheckButton = new Button(group, SWT.CHECK);
-		btnCheckButton.setText("Check Button");
-		new Label(group, SWT.NONE);
-		new Label(group, SWT.NONE);
-		
-		Label label_1 = new Label(group, SWT.SEPARATOR | SWT.HORIZONTAL);
-		new Label(group, SWT.NONE);
-		new Label(group, SWT.NONE);
-		new Label(group, SWT.NONE);
-		
-		Button btnRadioButton = new Button(group, SWT.RADIO);
-		btnRadioButton.setText("Radio Button");
-		
-		Spinner spinner = new Spinner(group, SWT.BORDER);
-		
-		Label label = new Label(group, SWT.SEPARATOR | SWT.HORIZONTAL);
-		
-		DateTime dateTime = new DateTime(group, SWT.BORDER);
-		
-		table = new Table(group, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-		
-		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn_1.setWidth(100);
-		tblclmnNewColumn_1.setText("New Column");
-		
-		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn.setWidth(100);
-		tblclmnNewColumn.setText("New Column");
-		new Label(group, SWT.NONE);
-		
-		Scale scale = new Scale(group, SWT.NONE);
-		
-		Slider slider = new Slider(group, SWT.NONE);
-		
-		Link link = new Link(group, SWT.NONE);
-		link.setText("<a>New Link</a>");
-		setRental(agence.getRentals().get(0));
+		/*
+		 * group = new Group(parent, SWT.NONE); group.setLayout(new GridLayout(6,
+		 * false)); group.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+		 * 1, 1));
+		 * 
+		 * Label lblNewLabel = new Label(group, SWT.NONE); lblNewLabel.setLayoutData(new
+		 * GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		 * lblNewLabel.setText("New Label");
+		 * 
+		 * text = new Text(group, SWT.BORDER); text.setLayoutData(new GridData(SWT.FILL,
+		 * SWT.CENTER, true, false, 1, 1)); new Label(group, SWT.NONE);
+		 * 
+		 * Combo combo = new Combo(group, SWT.NONE); combo.setLayoutData(new
+		 * GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		 * 
+		 * Button btnNewButton = new Button(group, SWT.NONE);
+		 * btnNewButton.setText("New Button");
+		 * 
+		 * Button btnCheckButton = new Button(group, SWT.CHECK);
+		 * btnCheckButton.setText("Check Button"); new Label(group, SWT.NONE); new
+		 * Label(group, SWT.NONE);
+		 * 
+		 * Label label_1 = new Label(group, SWT.SEPARATOR | SWT.HORIZONTAL); new
+		 * Label(group, SWT.NONE); new Label(group, SWT.NONE); new Label(group,
+		 * SWT.NONE);
+		 * 
+		 * Button btnRadioButton = new Button(group, SWT.RADIO);
+		 * btnRadioButton.setText("Radio Button");
+		 * 
+		 * Spinner spinner = new Spinner(group, SWT.BORDER);
+		 * 
+		 * Label label = new Label(group, SWT.SEPARATOR | SWT.HORIZONTAL);
+		 * 
+		 * DateTime dateTime = new DateTime(group, SWT.BORDER);
+		 * 
+		 * table = new Table(group, SWT.BORDER | SWT.FULL_SELECTION);
+		 * table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		 * table.setHeaderVisible(true); table.setLinesVisible(true);
+		 * 
+		 * TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
+		 * tblclmnNewColumn_1.setWidth(100); tblclmnNewColumn_1.setText("New Column");
+		 * 
+		 * TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
+		 * tblclmnNewColumn.setWidth(100); tblclmnNewColumn.setText("New Column"); new
+		 * Label(group, SWT.NONE);
+		 * 
+		 * Scale scale = new Scale(group, SWT.NONE);
+		 * 
+		 * Slider slider = new Slider(group, SWT.NONE);
+		 * 
+		 * Link link = new Link(group, SWT.NONE); link.setText("<a>New Link</a>");
+		 */
+		//setRental(agence.getRentals().get(0));
 	}
-	
-	 
-	public void setRental(Rental agence) {
-		lblNom.setText(agence.getCustomer().getFirstName());
-		lblPrenom.setText(agence.getCustomer().getLastName());
-		lblObjet.setText(agence.getRentedObject().getName());
-		lblDatedebut.setText(String.format("%d/%d/%d",
-				agence.getStartDate().getDay(),
-				agence.getStartDate().getMonth(),
-				agence.getStartDate().getYear()+1900));
-		lblDatefin.setText(String.format("%d/%d/%d",
-				agence.getEndDate().getDay(),
-				agence.getEndDate().getMonth(),
-				agence.getEndDate().getYear()+1900));
+
+	@Inject
+	@Optional
+	public void setRental(@Named(IServiceConstants.ACTIVE_SELECTION) Rental rental) {
+		if (rental != null) {
+			lblNom.setText(rental.getCustomer().getFirstName());
+			lblPrenom.setText(rental.getCustomer().getLastName());
+			lblObjet.setText(rental.getRentedObject().getName());
+			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+			
+			lblDatedebut.setText(df.format(rental.getStartDate()));
+			lblDatefin.setText(df.format(rental.getEndDate()));
+		}
 	}
-	
+
 	@Focus
 	private void setFocus() {
 		lblNom.setFocus();
