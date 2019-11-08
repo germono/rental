01069603fde9d7428e31f2ecfce72f0a79f255ce
Interindep <Interindep@DESKTOP-29VVM8O>
2019-11-08 14:52:10 +0100

    final

--- oliv.exo.rental.ui/src/oliv/exo/rental/ui/addon/InitAgence.java
+++ oliv.exo.rental.ui/src/oliv/exo/rental/ui/addon/InitAgence.java
@@ -26,7 +26,11 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 import com.opcoach.e4.preferences.ScopedPreferenceStore;
+import com.opcoach.training.rental.Address;
+import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalFactory;
+import com.opcoach.training.rental.StreetType;
 
 import oliv.exo.rebtal.core.RentalCoreActivator;
 import oliv.exo.rental.ui.RentalUIConstantes;
@@ -40,6 +44,20 @@ public class InitAgence implements RentalUIConstantes {
 	@PostConstruct
 	public void initAgence(IEclipseContext context, IExtensionRegistry reg,@Preference(value="PalettePreference") String nomPalette) {
 		RentalAgency courante = RentalCoreActivator.getAgence();
+		RentalFactory f = RentalFactory.eINSTANCE;
+		 Address ad1 = f.createAddress();
+		    ad1.setNumber(12);
+		    ad1.setStreetType(StreetType.ROAD);
+		    ad1.setStreetName("des bois");
+		    ad1.setZipCode("31400");
+		    ad1.setCity("Toulouse");
+		for (int i = 0; i < 0; i++) {
+			Customer c1 = f.createCustomer();
+		    c1.setFirstName("John");
+		    c1.setLastName("Wayne "+i);
+		    c1.setAddress(ad1);
+		    courante.addCustomer(c1);
+		}
 		context.set(AGENCE_COURANTE, courante);
 		context.set(RENTAL_UI_IMG_REGISTRE, getLocalImageRegistry());
 		context.set("PREF", new ScopedPreferenceStore(InstanceScope.INSTANCE, "oliv.exo.rental.ui"));
