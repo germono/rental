285b56da941459e68858ecc24e808217d6a11144
Interindep <Interindep@DESKTOP-29VVM8O>
2019-11-08 11:20:09 +0100

    Tree virtuel ok

--- oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalAngencyTree2.java
+++ oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalAngencyTree2.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
@@ -19,11 +21,13 @@ import oliv.exo.rental.ui.RentalUIConstantes;
 public class RentalAngencyTree2 implements RentalUIConstantes{
 
 	@PostConstruct
-	public void initArbre(Composite parent, @Named(AGENCE_COURANTE) RentalAgency agence) {
+	public void initArbre(Composite parent, @Named(AGENCE_COURANTE) RentalAgency agence,IEclipseContext context) {
 		TreeViewer tv  = new TreeViewer(parent,SWT.VIRTUAL);
 		tv.setUseHashlookup(true);
-		tv.setContentProvider(new RentalProvider2(tv));
-		tv.setLabelProvider((RentalProvider2)tv.getContentProvider());
+		context.set("ARBRE", tv);
+		RentalProvider2 rp = ContextInjectionFactory.make(RentalProvider2.class,context);
+		tv.setContentProvider(rp);
+		tv.setLabelProvider(rp);
 		List root=Arrays.asList(agence);
 		tv.setInput(root);
 		tv.getTree().setLayoutData(GridDataFactory.fillDefaults().create());
--- oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalProvider2.java
+++ oliv.exo.rental.ui/src/oliv/exo/rental/ui/view/RentalProvider2.java
@@ -2,24 +2,52 @@ package oliv.exo.rental.ui.view;
 
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.jface.resource.ColorRegistry;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.StringConverter;
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ILazyTreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
+import com.opcoach.e4.preferences.ScopedPreferenceStore;
 import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
+
+import oliv.exo.rental.ui.RentalUIConstantes;
 
 
-public class RentalProvider2 extends LabelProvider implements ILazyTreeContentProvider{
+
+
+
+
+public class RentalProvider2 extends LabelProvider implements ILazyTreeContentProvider,IColorProvider,RentalUIConstantes{
 	
 	private TreeViewer fViewer;
 	private Object[] nodes;
+	@Inject @Named(RENTAL_UI_IMG_REGISTRE)
+	private ImageRegistry bankImg;
 	
-	public RentalProvider2(TreeViewer fViewer) {
+	
+	private ScopedPreferenceStore pref;
+	
+	@Inject
+	public RentalProvider2(@Named("PREF")ScopedPreferenceStore isc,@Named("ARBRE")TreeViewer fViewer) {
 		
 		this.fViewer=fViewer;
+		pref=isc; 
 	}
-
+	
 	@Override
 	public Object getParent(Object element) {
 		System.out.println("Parent?"+element.getClass().getName());
@@ -36,21 +64,27 @@ public class RentalProvider2 extends LabelProvider implements ILazyTreeContentPr
 			return ((RentalAgency) element).getName();
 		if(element instanceof Customer) 
 			return ((Customer) element).getDisplayName();		
+		if(element instanceof RentalObject) 
+			return ((RentalObject) element).getName();		
 		return element.toString();
 	}
 
 	@Override
 	public void updateElement(Object parent, int index) {
-		System.out.println(parent.getClass().getName());
+//		System.out.println(parent.getClass().getName());
 		Object element;
 		if(parent instanceof List) {
 			element=((List) parent).get(index);
-//		}else if(parent instanceof RentalAgency) {
-//			fViewer.setChildCount(element, 3);
-//			return;
-//		}else if(parent instanceof Node) {
-//			fViewer.setChildCount(element,((Node) element).getChildren().length);
-//			return;
+		}else if(parent instanceof RentalAgency) {
+			if(nodes==null) {
+				nodes=new Object[] {new Node(Node.CUSTOMERS,(RentalAgency)parent),
+						new Node(Node.LOCATIONS,(RentalAgency)parent),
+						new Node(Node.OBJET,(RentalAgency)parent)};
+			}
+			element=nodes[index];
+			
+		}else if(parent instanceof Node) {
+			element=((Node) parent).getChildren()[index];
 		}else {
 			element=null;
 		}
@@ -69,7 +103,7 @@ public class RentalProvider2 extends LabelProvider implements ILazyTreeContentPr
 	public void updateChildCount(Object element, int currentChildCount) {
 		//System.out.println(element + " " + ((Node) element).getChildCount());
 		//fViewer.setChildCount(element, ((Node) element).getChildCount());
-		System.out.println("Con"+element.getClass().getName());
+//		System.out.println("Con"+element.getClass().getName());
 		if(element instanceof List) {
 			fViewer.setChildCount(element, ((List)element).size());
 		}
@@ -143,5 +177,51 @@ public class RentalProvider2 extends LabelProvider implements ILazyTreeContentPr
 			return RentalProvider2.this;
 		}
 		
+	}
+	
+	@Override
+	public Color getForeground(Object element) {
+		if(element instanceof RentalAgency) 
+			return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_BLUE);
+		if(element instanceof Customer) 			
+			return getAColor(pref.getString(PREF_COLOR_CUSTUMER));
+		if(element instanceof Rental) 
+			return getAColor(pref.getString(PREF_COLOR_RENTAL));
+		if(element instanceof Node) 
+				return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_MAGENTA);
+		if(element instanceof RentalObject) 
+			return  getAColor(pref.getString(PREF_COLOR_OBJECT));
+		return null;
+	}
+	
+	private Color getAColor(String rgbKey) {
+		ColorRegistry colorRegistry = JFaceResources.getColorRegistry();
+		Color col = colorRegistry.get(rgbKey);
+		if (col==null) {
+			colorRegistry.put(rgbKey, StringConverter.asRGB(rgbKey));
+			col=colorRegistry.get(rgbKey);
+		}
+		return col;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+		}
+	@Override
+	public Image getImage(Object element) {
+//		System.out.println(bankImg);
+		if(element instanceof RentalAgency) 
+			return bankImg.get(IMG_AGENCY);
+		if(element instanceof Customer) 
+			return bankImg.get(IMG_CUSTO);
+		if(element instanceof Rental) 
+			return bankImg.get(IMG_RENTAL);
+		if(element instanceof Node) 
+			return bankImg.get(IMG_SAMPLE);
+		if(element instanceof RentalObject) 
+			return bankImg.get(IMG_RENTAL_OBJET);
+		return null;
 	}
 }
